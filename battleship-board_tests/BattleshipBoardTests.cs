645ed081effb24b728b606c5adff15e5c6fc3207
Adam Kinnell <adamkinnell13@gmail.com>
2019-02-06 20:32:07 +1100

    Environment setup complete

---
+++ battleship-board_tests/BattleshipBoardTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using battleship_board;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace battleship_board.Tests
+{
+    [TestClass()]
+    public class BattleshipBoardTests
+    {
+        [TestMethod()]
+        public void lolTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
